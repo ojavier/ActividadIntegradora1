d840b198ca2e6b713a1dba01286bcc14be56f12c
ojavier <ojvilleda6@gmail.com>
2024-04-25 23:50:52 -0600

    correción main

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -30,7 +30,7 @@ def kmp_search(pat, txt):
     M = len(pat)
     N = len(txt)
     if M == 0 or N == 0:
-        return -1
+        return False, -1  # Cambio aquí, devolvemos False y -1 si no se encuentra el patrón
     
     long = calcular_longitud(pat)  # Calcula Longitud para el patrón
     i = 0
@@ -40,14 +40,15 @@ def kmp_search(pat, txt):
             i += 1
             j += 1
         if j == M:
-            return i-j  # Devuelve la posición donde se encontró el patrón en el archivo transmission
+            return True, i - j  # Cambio aquí, devolvemos True y la posición donde se encontró el patrón
             j = long[j-1]  # Ajusta j basado en la longitud previamente calculada
         elif i < N and pat[j] != txt[i]:
             if j != 0:
                 j = long[j-1]  
             else:
                 i += 1
-    return -1
+    return False, -1  # Cambio aquí, devolvemos False y -1 si no se encuentra el patrón
+
 
 # Función para encontrar el palíndromo más largo en una cadena
 def encontrar_palindromo(cadena):
@@ -68,8 +69,8 @@ def encontrar_codigo_transmision(mcode_archivos, transmission_archivos):
             posiciones = []
             inicio = 0
             while True:
-                pos = kmp_search(mcode_contenido_hex, transmission_contenido_hex[inicio:])
-                if pos != -1:
+                found, pos = kmp_search(mcode_contenido_hex, transmission_contenido_hex[inicio:])
+                if found:
                     inicio += pos + 1
                     fin = inicio + len(mcode_contenido_hex)
                     posiciones.append((inicio, fin))
@@ -83,68 +84,20 @@ def encontrar_codigo_transmision(mcode_archivos, transmission_archivos):
                 print("false", end=" ")
         print()
 
-class SuffixTree:
-    def __init__(self, cadena):
-        self.cadena = cadena
-        self.raiz = {}
-        self._construir_arbol()
-
-    def _agregar_sufijo(self, sufijo, pos):
-        nodo_actual = self.raiz
-        for char in sufijo:
-            if char not in nodo_actual:
-                nodo_actual[char] = {}
-            nodo_actual = nodo_actual[char]
-        nodo_actual['pos'] = pos
-
-    def _construir_arbol(self):
-        n = len(self.cadena)
-        for i in range(n):
-            self._agregar_sufijo(self.cadena[i:], i)
-
-    def buscar_subcadena(self, subcadena):
-        nodo_actual = self.raiz
-        for char in subcadena:
-            if char in nodo_actual:
-                nodo_actual = nodo_actual[char]
-            else:
-                return []
-        return self._encontrar_posiciones(nodo_actual)
-
-    def _encontrar_posiciones(self, nodo):
-        if 'pos' in nodo:
-            return [nodo['pos']]
-        posiciones = []
-        for hijo in nodo.values():
-            posiciones.extend(self._encontrar_posiciones(hijo))
-        return posiciones
-
-# Función para encontrar todas las ocurrencias del substring más largo común entre archivos de transmisión
-def encontrar_todas_ocurrencias_substring_comun_archivos(transmission_archivo1, transmission_archivo2):
-    transmission_contenido1 = leer_archivo(transmission_archivo1)
-    transmission_contenido2 = leer_archivo(transmission_archivo2)
-    sufijo1 = SuffixTree(transmission_contenido1)
-    sufijo2 = SuffixTree(transmission_contenido2)
-    
-    ocurrencias1 = []
-    ocurrencias2 = []
-    
-    for i in range(len(transmission_contenido1)):
-        for j in range(i + 1, len(transmission_contenido1) + 1):
-            subcadena = transmission_contenido1[i:j]
-            posiciones = sufijo2.buscar_subcadena(subcadena)
-            if posiciones:
-                ocurrencias1.append((i + 1, j))
-    
-    for i in range(len(transmission_contenido2)):
-        for j in range(i + 1, len(transmission_contenido2) + 1):
-            subcadena = transmission_contenido2[i:j]
-            posiciones = sufijo1.buscar_subcadena(subcadena)
-            if posiciones:
-                ocurrencias2.append((i + 1, j))
-    
-    return max(ocurrencias1, key=lambda x: x[1] - x[0]), max(ocurrencias2, key=lambda x: x[1] - x[0])
-
+def longest_common_substring(txt1, txt2):
+    m = len(txt1)
+    n = len(txt2)
+    longest = 0
+    end_pos = 0
+    lookup = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if txt1[i - 1] == txt2[j - 1]:
+                lookup[i][j] = lookup[i - 1][j - 1] + 1
+                if lookup[i][j] > longest:
+                    longest = lookup[i][j]
+                    end_pos = i
+    return end_pos - longest + 1, end_pos
 
 def main():
     mcode_archivos = ["mcode01.txt", "mcode02.txt", "mcode03.txt"]
@@ -166,16 +119,26 @@ def main():
 
     # Parte 3
     print("~~~~~~~~~~~ PARTE 3 ~~~~~~~~~~~")
-    for i in range(len(transmission_archivos)):
-        for j in range(i + 1, len(transmission_archivos)):
-            ocurrencias1, ocurrencias2 = encontrar_todas_ocurrencias_substring_comun_archivos(transmission_archivos[i], transmission_archivos[j])
-            print(f"Archivo {i+1} y Archivo {j+1} (primer archivo -> segundo archivo):")
-            for inicio, fin in ocurrencias1:
-                print(f"({inicio}, {fin})")
-            print(f"Archivo {j+1} y Archivo {i+1} (segundo archivo -> primer archivo):")
-            for inicio, fin in ocurrencias2:
-                print(f"({inicio}, {fin})")
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    # Búsqueda del substring más largo común
+    for mcode_archivo in mcode_archivos:
+        mcode = leer_archivo(mcode_archivo)
+        for transmission_archivo in transmission_archivos:
+            transmission = leer_archivo(transmission_archivo)
+            found, pos = kmp_search(mcode, transmission)
+            if found:
+                print("True", end=" ")
+                print(pos + 1)  # Corrección aquí, sumamos 1 a pos para obtener la posición correcta
+
+    for transmission_archivo in transmission_archivos:
+        transmission = leer_archivo(transmission_archivo)
+        transmission1 = leer_archivo(transmission_archivos[0])
+        start, end = longest_common_substring(transmission1, transmission)
+        print(start, end, end=" ")
+
+    transmission1 = leer_archivo(transmission_archivos[0])
+    transmission2 = leer_archivo(transmission_archivos[1])
+    start, end = longest_common_substring(transmission1, transmission2)
+    print(start, end)
 
 
 if __name__ == "__main__":
